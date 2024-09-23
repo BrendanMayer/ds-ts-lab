0e26b66d985698cc164ccab7dcfc127fc40dcf3c
BMayerWIT <20097866@mail.wit.ie>
2024-09-23 10:19:55 +0100

    Complete type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -15,6 +15,10 @@ interface Colleague {
     contact: Contact;
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
 
 const friend1: Friend = {
     name: "Arthur Bylik",
@@ -58,7 +62,8 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
