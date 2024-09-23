4acd8133aacbea1202ec88d1fe5f45de273b15db
BMayerWIT <20097866@mail.wit.ie>
2024-09-23 11:45:48 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -51,8 +51,7 @@ function addColleague(cs: Colleague[], name: string, department: string, email: 
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number,
-    max? : number
+    sorter: (c1: Colleague, c2: Colleague) => number, max? : number
   ): EmailContact[] {
     let end = colleagues.length;
     if (max !== undefined) {
@@ -63,14 +62,27 @@ function sortColleagues(
     return fullResult.slice(0,end)
   }
   // Test invocations
-  console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
+    //console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+    //console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+    //console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
 
 
 function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean): string[] {
     return friends.filter(criterion).map(friend => friend.name);
 }
+
+function addInterest(friend: Friend, interest: string): string[] {
+    
+    if (friend.interests == undefined) {
+        friend.interests = [];
+    }
+    
+    friend.interests.push(interest);
+    
+    return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
 
 //console.log(findFriends(friends, (friend) => friend.name.startsWith('Ar')));
 //console.log(findFriends(friends, (friend) => friend.age < 35));
